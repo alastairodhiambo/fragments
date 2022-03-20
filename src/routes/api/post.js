9f94be9c857e560f045f71f3bff500deb8cda105
Alastair Odhiambo <alastairodhiambo@outlook.com>
2022-03-19 22:45:25 -0400

    Add Location to POST request

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -6,6 +6,7 @@ module.exports = async (req, res, next) => {
   const data = req.body;
   const user = req.user;
   const type = req.headers['content-type'];
+  const host = req.headers['host'];
 
   logger.debug({ user, data, type, req }, 'POST request:');
 
@@ -13,7 +14,10 @@ module.exports = async (req, res, next) => {
     const fragment = new Fragment({ ownerId: user, type: type });
     await fragment.save();
     await fragment.setData(data);
+    const id = fragment.id;
+    logger.debug(id, 'POST fragment id');
 
+    res.location(`http://${host}/v1/fragments/${id}`);
     res.status(201).json(createSuccessResponse({ fragment }));
   } catch (err) {
     next(err);
