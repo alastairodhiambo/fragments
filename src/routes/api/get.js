d882ee68ab62139619eee31f2df6c4c0c279e0cf
Alastair Odhiambo <alastairodhiambo@outlook.com>
2022-03-19 22:46:10 -0400

    refactor: move GET requests to get.js file

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -5,7 +5,7 @@ const { createSuccessResponse } = require('../../response');
 /**
  * Get a list of fragments for the current user
  */
-module.exports = async (req, res) => {
+module.exports.getRoute = async (req, res) => {
   const user = req.user;
   const expand = req.query.expand || 0;
 
@@ -14,4 +14,37 @@ module.exports = async (req, res) => {
   logger.debug({ fragments, expand }, 'List Fragments, Expand');
 
   res.status(200).json(createSuccessResponse({ fragments }));
+};
+
+// GET /fragments/:id returns an existing fragment's data with the expected `Content-Type`, with unit tests. See 4.5.
+module.exports.getById = async (req, res, next) => {
+  const user = req.user;
+  const id = req.params.id;
+
+  logger.debug({ user, id }, 'GET /:ID User and ID');
+
+  try {
+    const fragment = await Fragment.byId(user, id);
+    const data = (await fragment.getData()).toString();
+    logger.debug(data, '/fragments/:id getData()');
+
+    res.status(200).send(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET /fragments/:id/info returns an existing fragment's metadata
+module.exports.getInfo = async (req, res, next) => {
+  const user = req.user;
+  const id = req.params.id;
+
+  logger.debug({ user, id }, 'GET /:ID User and ID');
+
+  try {
+    const data = await Fragment.byId(user, id);
+    res.status(200).json(createSuccessResponse({ message: data }));
+  } catch (err) {
+    next(err);
+  }
 };
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -5,44 +5,17 @@ const express = require('express');
 
 const contentType = require('content-type');
 const { Fragment } = require('../../model/fragment');
-const logger = require('../../logger');
-const { createSuccessResponse } = require('../../response');
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
-// Define our first route, which will be: GET /v1/fragments
-router.get('/fragments', require('./get'));
+const { getRoute, getById, getInfo } = require('./get');
+const route = '/fragments';
 
-// TODO: `GET /fragments/:id` returns an existing fragment's data with the expected `Content-Type`, with unit tests. See 4.5.
-
-router.get('/fragments/:id', async (req, res, next) => {
-  const user = req.user;
-  const id = req.params.id;
-
-  logger.debug({ user, id }, 'GET /:ID User and ID');
-
-  try {
-    const fragment = await Fragment.byId(user, id);
-    const data = (await fragment.getData()).toString();
-    logger.debug(data, '/fragments/:id getData()');
-
-    res.status(200).send(data);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/fragments/:id/info', async (req, res) => {
-  const user = req.user;
-  const id = req.params.id;
-
-  logger.debug({ user, id }, 'GET /:ID User and ID');
-
-  const data = await Fragment.byId(user, id);
-
-  res.status(200).json(createSuccessResponse({ message: data }));
-});
+// GET routes
+router.get(route, getRoute);
+router.get(`${route}/:id`, getById);
+router.get(`${route}/fragments/:id/info`, getInfo);
 
 // TODO: `GET /fragments/:id.ext` returns an existing fragment's data converted to a supported type.
 // Initial, you only need to support Markdown fragments (`.md`) converted to HTML (`.html`) using [markdown-it](https://github.com/markdown-it/markdown-it) (i.e., you don't have to do other conversions until [Assignment 3](https://github.com/humphd/ccp555-winter-2022/blob/main/assignments/assignment-03/README.md))
